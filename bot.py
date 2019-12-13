a1af9760a8f2e697fd7e04a66de1f74e717328cd
unknown <max.magill@yahoo.co.uk>
2019-12-13 02:09:41 +0000

    idk

--- bot.py
+++ bot.py
@@ -9,6 +9,8 @@ load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
+startup_extensions = [commands]
+
 bot = commands.Bot(command_prefix="!")
 
 @bot.event
@@ -17,6 +19,16 @@ async def on_ready():
         f'{bot.user} has connected to discord'
     )
 
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if message.content == "hatsune miku":
+        await message.channel.send("hatsune miku")
+
+    if message.content == "me":
+        await message.channel.send("pussy")
 @bot.command(name="gay")
 async def gay(ctx):
     num = random.choice(range(1, 102))
@@ -25,5 +37,12 @@ async def gay(ctx):
 @bot.command(name="test")
 async def test(ctx):
     await ctx.send("shut up f*g")
-    
+
+@bot.command(name="shutdown")
+@commands.has_role("galaxy")
+async def shutdown(ctx):
+
+    ctx.send("shutting down")
+    bot.logout()
+
 bot.run(token)

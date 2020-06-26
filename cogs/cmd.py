d3eac9dfb4788758fe5fdbb83fa04ff9b72f7501
Max Magill <maxmagill@Maxs-MacBook-Air.local>
2020-06-26 03:48:43 +0100

    move cogs into cog folder

---
+++ cogs/cmd.py
@@ -0,0 +1,43 @@
+import cogs
+import discord
+import random
+from discord.ext import commands
+
+
+class Cmd(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self._last_member = None
+
+    @commands.command()
+    async def gay(self, ctx):
+        await ctx.send("%s is %s%% gay!"%(ctx.author.mention, random.choice(range(1, 102))))
+        
+    @commands.command(name="test")
+    async def test(self, ctx):
+        await ctx.send("test")
+
+    @commands.command(name="piss")
+    async def piss(self, ctx):
+        await ctx.send("https://media.discordapp.net/attachments/602966702191280139/617107230269112331/PISS.gif")
+    
+    @commands.command(name="mikufancam")
+    async def mikucam(self, ctx):
+        await ctx.send("https://www.youtube.com/watch?v=KNrdGx69pCo")
+
+    @commands.command(name="bonk")
+    async def bonk(self, ctx):
+        await ctx.send("https://cdn.discordapp.com/attachments/611577151623528449/660896781919322125/bonk.mp4")
+        
+    # @commands.command(name="setcolor")
+    # async def setcolor(self, ctx, arg):
+    #     author = ctx.message.author
+    #     await self.bot.create_role(author.server, name="arg", colour=discord.Colour(arg))
+
+    #     user = ctx.message.author
+    #     role = discord.utils.get(user.server.roles, name=arg)
+    #     await self.bot.add_roles(user, role)
+
+
+def setup(bot):
+    bot.add_cog(Cmd(bot))
---
+++ cogs/moderation.py
@@ -0,0 +1,35 @@
+import cogs
+import discord
+import random
+from discord.ext import commands
+
+class Moderation(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self._last_member = None
+
+    @commands.command()
+    async def cmd(self, ctx):
+        embed=discord.Embed(title=" ", color=0xa985f9)
+        embed.set_author(name="Comamnds")
+        embed.set_thumbnail(url="https://cdn.discordapp.com/emojis/654614324509409300.png?v=1")
+        embed.add_field(name="!piss", value="Posts piss.gif", inline=False)
+        embed.add_field(name="!gay", value="Tells you how gay you are", inline=False)
+        embed.add_field(name="!mikufancam", value="Miku Fan Cam", inline=False)
+        embed.set_footer(text="Use !noncmd for non command responses!")
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def noncmd(self, ctx):
+        embed=discord.Embed(title=" ", description="Commands that do no require a ! prefix", color=0x91f9a3)
+        embed.set_author(name="Non-commands")
+        embed.set_thumbnail(url="https://cdn.discordapp.com/emojis/654614324509409300.png?v=1")
+        embed.add_field(name="me", value="pussy", inline=False)
+        embed.add_field(name="and thats on what?", value="periodt, luv", inline=False)
+        embed.add_field(name="hatsune miku", value="hatsune miku", inline=False)
+        embed.add_field(name="straight pride", value="straight pride?? HAHAHAHAHA", inline=True)
+        embed.set_footer(text="Use !cmd for all commands!")
+        await ctx.send(embed=embed)
+
+def setup(bot):
+    bot.add_cog(Moderation(bot))
